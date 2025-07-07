ec87007032945bb290d17efe8a025fe7957311e1
edumu <eduardoaum2007@gmail.com>
2025-07-07 08:45:43 -0300

    OOP first ativ

---
+++ OOP/src/Main.java
@@ -0,0 +1,24 @@
+//Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe.
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rectangle width and height: ");
+        Product rectangle = new Product();
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f\n", rectangle.area());
+        System.out.printf("PERIMETER = %.2f\n", rectangle.perimeter());
+        System.out.printf("DIAGONAL = %.2f\n", rectangle.diagonal());
+
+        sc.close();
+    }
+}
